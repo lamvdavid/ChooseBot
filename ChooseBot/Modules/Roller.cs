3ab7bff6ab100b8ee3a70e9b8bbe08ae723c4516
lamvd <lamvdavid@gmail.com>
2021-07-29 16:55:51 -0400

    Add project files.

---
+++ ChooseBot/Modules/Roller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ChooseBot.Modules
+{
+    public class Roller : ModuleBase<SocketCommandContext>
+    {
+        Random rnd = new Random();
+
+        [Command("flip")]
+        [Summary("Flip a coin")]
+        public async Task Flip()
+        {
+            if(rnd.Next(2) == 0)
+            {
+                await ReplyAsync("Heads");
+            } else
+            {
+                await ReplyAsync("Tails");
+            }
+            
+        }
+    }
+}
---
+++ ChooseBot/Program.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace ChooseBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var services = ConfigureServices();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _client.Log += Log;
+            services.GetRequiredService<CommandService>().Log += Log;
+
+            var token = Environment.GetEnvironmentVariable("TOKEN");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await services.GetRequiredService<CommandHandler>().InstallCommandsAsync();
+
+            await Task.Delay(-1);
+        }
+        public ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    MessageCacheSize = 500,
+                    LogLevel = LogSeverity.Info
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    LogLevel = LogSeverity.Info,
+                    DefaultRunMode = RunMode.Async,
+                    CaseSensitiveCommands = false
+                }))
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
