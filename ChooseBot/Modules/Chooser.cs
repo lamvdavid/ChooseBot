adb33636d07d1f5c3b7ce37744ba88161c41a863
lamvd <lamvdavid@gmail.com>
2021-07-29 17:21:30 -0400

    Add random choice selection

---
+++ ChooseBot/Modules/Chooser.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseBot.Modules
+{
+    public class Chooser : ModuleBase<SocketCommandContext>
+    {
+        readonly Random rnd = new Random();
+
+        [Command("choose")]
+        [Alias("c", "pick", "p")]
+        [Summary("Choose from a list")]
+        public async Task Choose([Remainder] string choices)
+        {
+            string[] split = choices.Split(',');
+
+            await ReplyAsync(split[rnd.Next(split.Length)]);
+        }
+    }
+}
